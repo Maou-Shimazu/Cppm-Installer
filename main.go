5915b5d7bd419297aefaa9ac9dc20c838a079ff8
BrandonDoesDev <93276748+BrandonDoesDev@users.noreply.github.com>
2022-06-04 11:08:20 -0400

    added version finding and exec downloader

--- main.go
+++ main.go
@@ -1,6 +1,62 @@
 package main
 
-// set a default location that is in the path by default and place/remove cppm from there if dosent exist or does.
+import (
+	"encoding/json"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"runtime"
+)
+
+type JSON struct {
+	Url string `json:"html_url"`
+}
+
 func main() {
-	println("Hello, World!")
+	vResp, err := http.Get("https://api.github.com/repos/maou-shimazu/cpp-project-manager/releases/latest")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer vResp.Body.Close()
+
+	vBytes, err := ioutil.ReadAll(vResp.Body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var vUrl JSON
+	json.Unmarshal(vBytes, &vUrl)
+
+	version := vUrl.Url[len(vUrl.Url)-6:]
+
+	out, err := os.Create("cppm-exec")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer out.Close()
+
+	os := runtime.GOOS
+	switch os {
+	case "windows":
+		resp, err := http.Get("https://github.com/Maou-Shimazu/Cpp-Project-Manager/releases/download/" + version + "/cppm-win-x64.exe")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer resp.Body.Close()
+		io.Copy(out, resp.Body)
+	case "linux":
+		resp, err := http.Get("https://github.com/Maou-Shimazu/Cpp-Project-Manager/releases/download/" + version + "/cppm-debian")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer resp.Body.Close()
+		io.Copy(out, resp.Body)
+	default:
+		log.Fatal("Unsupported OS")
+	}
 }
