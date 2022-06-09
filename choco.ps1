3bb4607a3c0daccafc2ba33ff06c6d6060ac2fae
Maou-Shimazu <eldaddanladi@gmail.com>
2022-06-08 22:29:25 -0600

    removed refresh

--- choco.ps1
+++ choco.ps1
@@ -1,10 +1,10 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
-Invoke-Command { & "powershell.exe" } -NoNewScope
+# Invoke-Command { & "powershell.exe" } -NoNewScope
 
 choco install msys2
 
-Invoke-Command { & "powershell.exe" } -NoNewScope
+# Invoke-Command { & "powershell.exe" } -NoNewScope
 
 $cppm = "$HOME/.cppm/bin"
 if (!(Test-Path -Path $cppm)) { # if cppm path dosent exist
@@ -14,6 +14,6 @@ if (!(Test-Path -Path $cppm)) { # if cppm path dosent exist
     [Environment]::SetEnvironmentVariable("PATH", $p + ";" + $cppm, [EnvironmentVariableTarget]::Machine);
 }
 
-Invoke-Command { & "powershell.exe" } -NoNewScope
+# Invoke-Command { & "powershell.exe" } -NoNewScope
 
 C:/msys64/msys2.exe bash msys2.sh
