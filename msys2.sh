66ee49253c941796e66043ac8d7e79b1bb17b832
Eldad Danladi <45318740+Maou-Shimazu@users.noreply.github.com>
2022-06-23 20:32:43 -0600

    Update msys2.sh

--- msys2.sh
+++ msys2.sh
@@ -1,3 +1,4 @@
 pacman -Syu --noconfirm
 pacman -S --needed base-devel mingw-w64-x86_64-toolchain --noconfirm
 pacman -S mingw-w64-x86_64-clang --noconfirm
+pacman -S mingw-w64-x86_64-clang-tools-extra --noconfirm
